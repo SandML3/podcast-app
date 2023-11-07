0eb35d34c8027bcd1bda6c361bf390863d68dc25
Sandra Martin <sandra.martin@kairosds.com>
2023-11-07 11:52:14 +0100

    fix: add allorigins url as fallback

--- src/infrastructure/http/httpClient.ts
+++ src/infrastructure/http/httpClient.ts
@@ -1,16 +1,23 @@
 import { ResponseDTO } from "./responseDto";
 
 async function get<T> (url: string): Promise<T> {
-    const allOriginsUrl = `https://api.allorigins.win/get?url=${encodeURIComponent(url)}`;
-    // const response = await fetch(url);
+    try {
+        const response = await fetch(url);
 
-    // return await response.json() as Promise<T>;
-    // if (response) return await response.json() as Promise<T>;
-
-    const allOriginsResponse = await fetch(allOriginsUrl);
-    const responseJson: ResponseDTO<T> = await allOriginsResponse.json();
+        if (response) return response.json() as Promise<T>;
+        
+        const allOriginsUrl = `https://api.allorigins.win/get?url=${encodeURIComponent(url)}`; 
     
-    return JSON.parse(responseJson.contents as string) as Promise<T>;  
+        const allOriginsResponse = await fetch(allOriginsUrl);
+        const responseJson: ResponseDTO<T> = await allOriginsResponse.json();
+        
+        
+        return JSON.parse(responseJson.contents as string) as Promise<T>;
+
+    } catch(error) {
+        console.log(error);
+        return Promise.reject(error);
+    }
 }
 
 export const http = { get };
--- src/infrastructure/http/httpPodcastsRepository.ts
+++ src/infrastructure/http/httpPodcastsRepository.ts
@@ -8,17 +8,19 @@ import { Entry, PodcastDTO } from './podcastDto';
 export const httpPodcastsRepository: PodcastsRepository = {
 
     async getAllPodcasts(): Promise<Podcast[]> {
+        
         const response = await http.get<PodcastDTO>('https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json');
-        // const podcastDto: PodcastDTO = JSON.parse(response.contents as string) as PodcastDTO;
-
-        const podcasts = response.feed.entry;
-        return podcasts.map((podcastDto: Entry) => ({
+    
+        const podcastsDto = response.feed.entry;
+        const podcasts = podcastsDto.map((podcastDto: Entry) => ({
             id: podcastDto.id.attributes['im:id'],
             title: podcastDto['im:name'].label,
             author: podcastDto['im:artist'].label,
             image: podcastDto['im:image'][2].label,
             summary: podcastDto.summary.label
-         }) )
+        }));
+
+        return podcasts;
     },
 
     async getPodcastEpisodes(podcastId: string, limit?: number): Promise<Episode[]> {
@@ -26,7 +28,6 @@ export const httpPodcastsRepository: PodcastsRepository = {
             ?limit
             :100;
         const response = await http.get<PocastDetailDTO>(`https://itunes.apple.com/lookup?id=${podcastId}&media=podcast&entity=podcastEpisode&limit=${resultsLimit}`);
-        // const podcastDetailDto: PocastDetailDTO = JSON.parse(response.contents as string) as PocastDetailDTO;
         
         const episodes: Episode[] = response.results
             .slice(1)
